11e2dc786d7efbcd9bcfbf33062cf24e95ce57b2
Mach-web <mkanderson771@gmail.com>
2023-07-15 10:53:31 +0300

    added comments.

--- practice2.py
+++ practice2.py
@@ -9,19 +9,25 @@ class PythonOrgSearch(unittest.TestCase):
 
     def test_search_in_python_org(self):
         driver = self.driver
+        # Get the website to be scraped
         driver.get("http://127.0.0.1:8000/")
+        # works like assert in python
         self.assertIn("DB", driver.title)
-
+        
+        '''find element by element by ID, or CSS_SELECTOR or XPATH,
+        then pass the values'''
         element = driver.find_element(By.ID, "content")
         element.send_keys("Hello by Id")
         # elem = driver.find_element(By.ID, "go").click()
 
         element = driver.find_element(By.CSS_SELECTOR, "#description")
         element.send_keys("Hello by CSS_SEECTOR")
+        # clear element contents
         element.clear()
 
         element = driver.find_element(By.XPATH, '//*[@id="description"]')
         element.send_keys("Hello There by Xpath")
+        # submit the form
         btn = driver.find_element(By.XPATH, '//*[@id="go"]').click()
         # element.submit()
 
@@ -31,6 +37,7 @@ class PythonOrgSearch(unittest.TestCase):
         #self.assertNotIn("No results found.", driver.page_source)
 
     def tearDown(self):
+        # close the webdriver
         self.driver.close()
 
 if __name__ == "__main__":
